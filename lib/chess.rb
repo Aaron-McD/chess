3407be4e161263ca04ff87af3b52955dc3c595db
Aaron McDonald <aaronmcdonald3d@gmail.com>
2020-08-12 13:07:00 -0400

    basic starting point for creating objects

---
+++ lib/chess.rb
@@ -0,0 +1,8 @@
+
+class Chess
+    def initialize(player1, player2)
+        @p1 = player1
+        @p2 = player2
+        puts "Hello world"
+    end
+end
---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative "lib/chess.rb"
+# example API usage
+puts "Welcome to chess in the console! We will start by getting the name of player 1: "
+# player1 = get_player
+puts "Alright now we will get the name of player 2:"
+# player2 = get_player
+
+game = Chess.new("player1", "player2")
+
+=begin
+until game.check_mate
+    game.change_player
+    game.show_board
+    game.play_round
+end
+=end
